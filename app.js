3368e89e5fda8c7966b053bab4e1a2bf5536e16b
Lidor Sissay <lidsis96w@gmail.com>
2026-03-05 19:49:01 +0200

    task-3: complex reduce

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-//1
+//3
 const getData = async url => fetch(url).then(response => response.json());
 (async () => {
     try {
-        const posts = await getData('https://jsonplaceholder.typicode.com/posts')
-        const showTitles = posts
-            .filter(({ userId }) => userId === 1)
-        console.log(showTitles)
-        document.getElementById('titles').innerHTML = showTitles
-            .map(({ id, title }) => `
-                <li>${id}: ${title}</li>
-            `)
-            .join('')
+        const users = await getData('https://jsonplaceholder.typicode.com/users')
+        const earthStatic = users
+            .reduce((acc, { address: { geo: { lat } } }) => {
+                if (+lat > 0) {
+                    acc.North += 1
+                } else {
+                    acc.South += 1
+                }
+                return acc
+            }, { North: 0, South: 0 })
+        const { North, South } = earthStatic
+        document.getElementById('sen').innerHTML = `
+            In the north: ${North} users.\nIn the south: ${South} users.
+        `
     } catch (error) { console.log(error) }
 })()

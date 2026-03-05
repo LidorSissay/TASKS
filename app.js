8c82d14e728c27717ca5dc146e6c53239c4acb84
Lidor Sissay <lidsis96w@gmail.com>
2026-03-05 19:13:14 +0200

    task-1: fixed js

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
-//2
-const getData = async url => fetch(url).then(response => response.json())
-const suiteUsers = async () => {
+//1
+const getData = async url => fetch(url).then(response => response.json());
+(async () => {
     try {
-        const users = await getData('https://jsonplaceholder.typicode.com/users')
-        document.getElementById('suite-users').innerHTML = users
-        .filter(({ address: { suite }}) => suite.includes('Suite') )
-        .map(({ address: { city }, id, name}) => `
-            <tr>
-                <td>${city}</td>
-                <td>${id}</td>
-                <td>${name}</td>
-            </tr>
-        `)
-        .join('')
-    } catch(err) { console.log(err) }
-}
-suiteUsers()
+        const posts = await getData('https://jsonplaceholder.typicode.com/posts')
+        const showTitles = posts
+            .filter(({ userId }) => userId === 1)
+        console.log(showTitles)
+        document.getElementById('titles').innerHTML = showTitles
+            .map(({ id, title }) => `
+                <li>${id}: ${title}</li>
+            `)
+            .join('')
+    } catch (error) { console.log(error) }
+})()

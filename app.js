0c5fd5ee975813927359339871b704f8ac8dde23
Lidor Sissay <lidsis96w@gmail.com>
2026-03-05 20:27:54 +0200

    quiz 1: comments logic

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-//3
+//quiz-1
 const getData = async url => fetch(url).then(response => response.json());
-(async () => {
+const postCom = async () => {
     try {
-        const users = await getData('https://jsonplaceholder.typicode.com/users')
-        const earthStatic = users
-            .reduce((acc, { address: { geo: { lat } } }) => {
-                if (+lat > 0) {
-                    acc.North += 1
-                } else {
-                    acc.South += 1
-                }
-                return acc
-            }, { North: 0, South: 0 })
-        const { North, South } = earthStatic
-        document.getElementById('sen').innerHTML = `
-            In the north: ${North} users.\nIn the south: ${South} users.
+        const comments = await getData('https://jsonplaceholder.typicode.com/comments')
+        const filteredPosts = comments
+            .filter(({ postId }) => +postId === 3)
+        const bodyLng = filteredPosts
+            .reduce((total, { body }) => {
+                total += body.length
+                return total
+            }, 0)
+        const average = bodyLng / filteredPosts.length
+        document.getElementById('sum').innerHTML = `
+            The average length of comments in post 3 is ${average} chars.
         `
     } catch (error) { console.log(error) }
-})()
+}
+postCom()
